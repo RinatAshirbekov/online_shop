4c09990e1171930891d0d3603319de3eefd1c4bc
Аширбеков Ринат <rinat.ashirbekov.90@mail.ru>
2020-06-30 00:41:05 +0600

    initial commit

---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="v-main-wrapper">
+      <v-catalog />
+      <v-cart />
+  </div>
+</template>
+
+<script>
+import vCatalog from './v-catalog'
+import vCart from './v-cart'
+
+export default {
+    name: 'v-main-wrapper',
+    components: {
+        vCatalog,
+        vCart
+    },
+    props: {},
+    data(){
+        return {
+            title: 'Main wrapper'
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+    watch: {
+
+    },
+};
+</script>
+
+<style>
+.v-main-wrapper {
+    /*display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-width: 900px;
+    margin: 0 auto;*/
+    min-width: 900px;
+    margin: 0 auto;
+}
+</style>
